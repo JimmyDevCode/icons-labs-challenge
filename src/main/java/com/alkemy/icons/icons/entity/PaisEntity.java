6d18d3cd63be37123b01f17a3a164d8cfc85ea86
JimmyDevCode <jimmy.rmp@hotmail.com>
2021-10-22 17:35:40 -0500

    Definicion de las entidades y sus relaciones

---
+++ src/main/java/com/alkemy/icons/icons/entity/PaisEntity.java
@@ -0,0 +1,40 @@
+package com.alkemy.icons.icons.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table (name="pais")
+public class PaisEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String denominacion;
+    @Column (name = "cant_habitantes")
+
+    private Long cantidadHabitantes;
+
+    private Long superficie;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "continente_id", insertable = false, updatable = false)
+    private ContinenteEntity continente;
+
+    @Column(name = "continente_id", nullable = false)
+    private Long continenteId;
+
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+
+            })
+    @JoinTable(
+            name = "icon_pais",
+            joinColumns = @JoinColumn(name= "pais_id"),
+            inverseJoinColumns = @JoinColumn(name = "icon_id"))
+    private Set<IconEntity> icons = new HashSet<>();
+}
